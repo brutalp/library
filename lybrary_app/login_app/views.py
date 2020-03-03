1854a4d80f9fd7fe2a1c879cabb117e3c6f027bc
brutalp <brutal616@gmail.com>
2020-03-03 22:40:33 +0300

    add method post

--- lybrary_app/login_app/views.py
+++ lybrary_app/login_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import HttpResponse
 
 
 class LoginView(View):
+
     def get(self, request):
         return render(request, 'login_app/index.html')
+
+    def post(self, request):
+        html = '<html><body>'
+        for key, value in request.POST.items():
+            html += f'{key}: {value}<br>'
+        html += '</html></body>'
+        return HttpResponse(html)
