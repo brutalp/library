4515c6176e8b110eb2703a708d05764a1e877549
brutalp <brutal616@gmail.com>
2020-04-18 18:38:45 +0300

    for test

---
+++ lab03/template-master/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { onMount, onDestroy } from 'svelte';
+    import Cookies from 'js-cookie/src/js.cookie.js'
+	const CSRF_TOKEN = Cookies.get('csrftoken');
+	const SHOP_URL = getRef('shop-ref');
+
+    let wines = [
+    ];
+
+    function getRef(id) {return document.getElementById(id).href;};
+    console.log(SHOP_URL);
+    onMount(async () => {
+        const response = await fetch(SHOP_URL, {
+                                        headers: {
+                                            'Accept': 'application/json, text-plain, */*',
+                                            'X-Requested-Width': 'XMLHttpRequest',
+                                        }, });
+        wines = await response.json();
+        console.log(wines);
+    });
+</script>
+
+<main>
+    {#each wines as item}
+
+    {/each}
+</main>
+
+<style>
+
+</style>
--- lab03/wine_app/views.py
+++ lab03/wine_app/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from wine_app.models import Wines, People
 from django.core.paginator import Paginator
 from django.urls import reverse
+import json
 
 
 class IndexView(View):
@@ -30,17 +32,37 @@ class AboutView(View):
         return render(request, 'wine_app/about.html')
 
 
+wine_count = 0
+
+
 class ShopView(View):
 
     def get(self, request, page_id=1):
         wines_list = Wines.objects.all()
-        paginator = Paginator(wines_list, 3)
-        try:
-            wines = paginator.page(page_id)
-            wines.num_pages_tuple = tuple(range(paginator.num_pages))
-        except:
-            return redirect(reverse('shop'))
-        return render(request, 'wine_app/shop.html', {'wines': wines})
+        if not request.is_ajax():
+            paginator = Paginator(wines_list, 3)
+            try:
+                wines = paginator.page(page_id)
+                wines.num_pages_tuple = tuple(range(paginator.num_pages))
+            except:
+                return redirect(reverse('shop'))
+            return render(request, 'wine_app/shop.html', {'wines': wines})
+        else:
+            print('###############################################################')
+            print(json.dumps({'wines': wines_list}))
+            print('###############################################################')
+            return HttpResponse(json.dumps({'wines': wines_list}), content_type='application/json')
+
+    # def post(self, request):
+    #     global wine_count
+    #     if request.is_ajax():
+    #         message = f'ShopListView: {wine_count}'
+    #         wine_count += 1
+    #         print(message)
+    #     else:
+    #         message = 'not AJAX request'
+    #
+    #     return HttpResponse(message)
 
 
 class ContactView(View):
