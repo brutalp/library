d5961d6715590b3abea22c0da78f1316099f2d2d
brutalp <brutal616@gmail.com>
2020-04-21 11:13:27 +0300

    bad fix(2)

--- lab03/template-master/src/App.svelte
+++ lab03/template-master/src/App.svelte
@@ -1,40 +1,43 @@
 <main>
     <div class="row">
-
-        {#each winesCut as item}
-            <div class="col-lg-4 mb-5 col-md-6">
-                <div class="wine_v_1 text-center pb-4">
-                    <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="{ item.img }" alt="Image" class="img-fluid"></a>
-                    <div>
-                        <h3 class="heading mb-1"><a href="#">{ item.title }</a></h3>
-                        <span class="price">{ item.price }</span>
+    {#each winesCut as item}
+        <div class="col-lg-4 mb-5 col-md-6">
+            <div class="wine_v_1 text-center pb-4">
+                <a href="shop-single.html" class="thumbnail d-block mb-4"><img src="{ item.img }" alt="Image" class="img-fluid"></a>
+                <div>
+                    <h3 class="heading mb-1"><a href="#">{ item.title }</a></h3>
+                    <span class="price">{ item.price }</span>
+                </div>
+                <div class="wine-actions">
+                    <h3 class="heading-2"><a href="#">{ item.title }</a></h3>
+                    <span class="price d-block">{ item.price }</span>
+                    <div class="rating">
+                        <span class="icon-star"></span>
+                        <span class="icon-star"></span>
+                        <span class="icon-star"></span>
+                        <span class="icon-star"></span>
+                        <span class="icon-star-o"></span>
                     </div>
-                    <div class="wine-actions">
-                        <h3 class="heading-2"><a href="#">{ item.title }</a></h3>
-                        <span class="price d-block">{ item.price }</span>
-                        <div class="rating">
-                            <span class="icon-star"></span>
-                            <span class="icon-star"></span>
-                            <span class="icon-star"></span>
-                            <span class="icon-star"></span>
-                            <span class="icon-star-o"></span>
-                        </div>
-                        <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span>Add to Cart</a>
-                    </div>
+                    <a href="cart.html" class="btn add"><span class="icon-shopping-bag mr-3"></span>Add to Cart</a>
                 </div>
             </div>
-        {/each}
-
-        <ul class="pagination">
-            {#each winePages as page}
-                <li>
-                    <button on:click="{() => sliceForPagination(page, wines)}">
-                        {page}
-                    </button>
-                </li>
-            {/each}
-        </ul>
-
+        </div>
+    {/each}
+    </div>
+    <div class="row">
+        <div class="col">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    {#each winePages as page}
+                        <li class="page-item">
+                            <button class="page-link" on:click="{() => sliceForPagination(page, wines)}">
+                                {page}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </nav>
+        </div>
     </div>
 </main>
 
@@ -68,7 +71,6 @@
     }
 
     function sliceForPagination(page, wines){
-        console.log(page)
         if (page == 1){
            winesCut = wines.slice(0, 3)
         } else if (page == 2){
@@ -76,7 +78,6 @@
         } else if (page == 3){
             winesCut = wines.slice(7, 10)
         } else {
-            console.log('при инициализации я залез сюда')
             winesCut = wines.slice(0, 3)
         }
         return winesCut
